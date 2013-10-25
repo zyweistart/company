90f0107e99e6cd0e6ba1485ed91c6cbac9fb9717
start <4544242@qq.com>
2013-10-25 14:00:53 +0800

    1

--- Navigation/gen/com/start/navigation/R.java
+++ Navigation/gen/com/start/navigation/R.java
@@ -37,9 +37,10 @@ public final class R {
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int activity_start=0x7f030001;
-        public static final int module_main_footer=0x7f030002;
-        public static final int module_main_header=0x7f030003;
-        public static final int widget_pull_to_refresh_head=0x7f030004;
+        public static final int lvitem_friendlocation=0x7f030002;
+        public static final int module_main_footer=0x7f030003;
+        public static final int module_main_header=0x7f030004;
+        public static final int widget_pull_to_refresh_head=0x7f030005;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
