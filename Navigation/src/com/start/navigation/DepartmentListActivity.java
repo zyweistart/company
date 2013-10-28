1b82a6a912fc079cd4d15ddf77b331ca7e1bf3a4
start <4544242@qq.com>
2013-10-28 17:53:32 +0800

    1

--- Navigation/src/com/start/navigation/DepartmentListActivity.java
+++ Navigation/src/com/start/navigation/DepartmentListActivity.java
@@ -6,7 +6,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.AdapterView.OnItemClickListener;
 
 import com.start.core.CoreActivity;
 import com.start.model.Department;
@@ -17,55 +21,75 @@ import com.start.service.DepartmentService;
  * @author start
  *
  */
-public class DepartmentListActivity extends CoreActivity {
+public class DepartmentListActivity extends CoreActivity implements OnItemClickListener{
 
 	
 	private List<Department> departments;
 	private DepartmentService departmentService;
 	
+	private ListView departmentListView;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_department_list);
+		
 		departmentService=new DepartmentService(this);
 		
 		departments=departmentService.findAll();
 		
+		DataAdapter adapter=new DataAdapter();
+		departmentListView=(ListView)findViewById(R.id.module_main_frame_department_list);
+		departmentListView.setAdapter(adapter);
+		departmentListView.setOnItemClickListener(this);
+		
 	}
 	
-	private class RecordedAdapter extends BaseAdapter{
+	private class DataAdapter extends BaseAdapter{
 
 		@Override
 		public int getCount() {
-			return 0;
+			return departments.size();
 		}
 
 		@Override
 		public Object getItem(int position) {
-			return null;
+			return departments.get(position);
 		}
 
 		@Override
 		public long getItemId(int position) {
-			return 0;
+			return position;
 		}
 		
 		@Override
 		public View getView(int position, View convertView, ViewGroup parent) {
 			ViewHolder holder;
-			if (convertView != null) {
+			if (convertView != null && convertView.getId() == R.id.lvitem_department_content) {
 				holder = (ViewHolder) convertView.getTag();
 			} else {
-				convertView = LayoutInflater.from(DepartmentListActivity.this).inflate(null, null);
+				convertView = LayoutInflater.from(DepartmentListActivity.this).inflate(R.layout.lvitem_department, null);
 				holder=new ViewHolder();
+				holder.tvName=(TextView)convertView.findViewById(R.id.lvitem_department_name);
+				
 				convertView.setTag(holder);
 			}
+			
+			Department department=departments.get(position);
+			holder.tvName.setText(department.getName());
 			return convertView;
 		}
 		
 		private class ViewHolder{
 			
+			private TextView tvName;
+			
 		}
+		
+	}
+
+	@Override
+	public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
 		
 	}
 
