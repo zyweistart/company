271697c1d1fe79592e3a1157f22ad27b430b75fc
Start <4544242@qq.com>
2013-12-01 21:36:50 +0800

    1

--- Navigation/src/com/start/navigation/MoreActivity.java
+++ Navigation/src/com/start/navigation/MoreActivity.java
@@ -1,10 +1,8 @@
 package com.start.navigation;
 
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.Button;
 
 import com.start.core.CoreActivity;
 
@@ -26,16 +24,17 @@ public class MoreActivity extends CoreActivity implements OnClickListener {
 	public void onClick(View v) {
 		if (v.getId() == R.id.more_btn_login) {
 			//用户登录
-
+			
 		} else if (v.getId() == R.id.more_btn_friends_manager) {
 			//好友管理
-
+			
 		} else if (v.getId() == R.id.more_btn_map_manager) {
 			//地图管理
+			
 		} else if (v.getId() == R.id.more_btn_version_check) {
 			//版本检测
+			
 		}
-
 	}
 
 }
