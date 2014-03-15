90a95c5ffdef9468095d6ba44ed3dc4fdd4923c6
Start <4544242@qq.com>
2014-03-15 12:37:13 +0800

    1

--- ElectricianRun/ElectricianRun/STChartElectricityValPieViewController.m
+++ ElectricianRun/ElectricianRun/STChartElectricityValPieViewController.m
@@ -54,15 +54,15 @@
     [pieView setBackgroundColor:[UIColor whiteColor]];
     [self.view addSubview:pieView];
     
-    MyPieElement* elem = [MyPieElement pieElementWithValue:(a) color:[UIColor greenColor]];
+    MyPieElement* elem = [MyPieElement pieElementWithValue:(a) color:[UIColor colorWithRed:(17/255.0) green:(69/255.0) blue:(202/255.0) alpha:1]];
     elem.title = @"谷电量";
     [pieView.layer addValues:@[elem] animated:NO];
     
-    MyPieElement* elem1 = [MyPieElement pieElementWithValue:(b) color:[UIColor blueColor]];
+    MyPieElement* elem1 = [MyPieElement pieElementWithValue:(b) color:[UIColor colorWithRed:(42/255.0) green:(180/255.0) blue:(13/255.0) alpha:1]];
     elem1.title = @"尖电量";
     [pieView.layer addValues:@[elem1] animated:NO];
     
-    MyPieElement* elem2 = [MyPieElement pieElementWithValue:(c) color:[UIColor redColor]];
+    MyPieElement* elem2 = [MyPieElement pieElementWithValue:(c) color:[UIColor colorWithRed:(241/255.0) green:(2/255.0) blue:(2/255.0) alpha:1]];
     elem2.title = @"峰电量";
     [pieView.layer addValues:@[elem2] animated:NO];
     
@@ -74,21 +74,21 @@
     UILabel *lbl=[[UILabel alloc]initWithFrame:CGRectMake(10, 350, 200, 20)];
     lbl.font=[UIFont systemFontOfSize:12.0];
     [lbl setText:[NSString stringWithFormat:@"谷电量：%.2f",a]];
-    [lbl setTextColor:[UIColor greenColor]];
+    [lbl setTextColor:[UIColor colorWithRed:(17/255.0) green:(69/255.0) blue:(202/255.0) alpha:1]];
     [lbl setBackgroundColor:[UIColor clearColor]];
     [lbl setTextAlignment:NSTextAlignmentLeft];
     [self.view addSubview:lbl];
     lbl=[[UILabel alloc]initWithFrame:CGRectMake(10, 375, 200, 20)];
     lbl.font=[UIFont systemFontOfSize:12.0];
     [lbl setText:[NSString stringWithFormat:@"尖电量：%.2f",b]];
-    [lbl setTextColor:[UIColor blueColor]];
+    [lbl setTextColor:[UIColor colorWithRed:(42/255.0) green:(180/255.0) blue:(13/255.0) alpha:1]];
     [lbl setBackgroundColor:[UIColor clearColor]];
     [lbl setTextAlignment:NSTextAlignmentLeft];
     [self.view addSubview:lbl];
     lbl=[[UILabel alloc]initWithFrame:CGRectMake(10, 400, 200, 20)];
     lbl.font=[UIFont systemFontOfSize:12.0];
     [lbl setText:[NSString stringWithFormat:@"峰电量：%.2f",c]];
-    [lbl setTextColor:[UIColor redColor]];
+    [lbl setTextColor:[UIColor colorWithRed:(241/255.0) green:(2/255.0) blue:(2/255.0) alpha:1]];
     [lbl setBackgroundColor:[UIColor clearColor]];
     [lbl setTextAlignment:NSTextAlignmentLeft];
     [self.view addSubview:lbl];
